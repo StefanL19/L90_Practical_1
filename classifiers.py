2cb07d0c81308e4e65695311bd60a75911120e6b
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2019-10-19 22:27:45 +0100

    Implemented Naive Bayes Classification and Laplace Smoothing

--- classifiers.py
+++ classifiers.py
@@ -2,6 +2,7 @@ import data_preprocessing
 from os import listdir
 from os.path import isfile, join
 from tqdm import tqdm
+import numpy as np
 
 def train_multinomial_NB(train_pos_data_path, train_neg_data_path,laplace_smoothing=False):
 	train_files_pos = [join(train_pos_data_path, f) for f in listdir(train_pos_data_path) if isfile(join(train_pos_data_path, f))]
@@ -44,9 +45,70 @@ def train_multinomial_NB(train_pos_data_path, train_neg_data_path,laplace_smooth
 
 	count_all_neg = sum(vocab_neg_freq)
 
-	vocab_pos_freq = [x / count_all_pos for x in vocab_pos_freq]
-	vocab_neg_freq = [x / count_all_neg for x in vocab_neg_freq]
+	if laplace_smoothing:
+		print("Using laplace laplace_smoothing")
+		vocab_pos_freq = [(x+1) / (count_all_pos+1) for x in vocab_pos_freq]
+		vocab_neg_freq = [(x+1) / (count_all_neg+1) for x in vocab_neg_freq]
+
+	else:
+		vocab_pos_freq = [x / count_all_pos for x in vocab_pos_freq]
+		vocab_neg_freq = [x / count_all_neg for x in vocab_neg_freq]
 
 	return vocab, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq
 
-def apply_multinomial_NP()
+def apply_multinomial_NB(doc, vocab, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq):
+	tokens = data_preprocessing.tokenize_text(doc)
+
+	bag = [0] * len(vocab)
+
+	for w in tokens:
+		for i, word in enumerate(vocab):
+			if word == w:
+				bag[i] = 1
+
+	score_pos = np.log(prior_pos)
+	score_neg = np.log(prior_neg)
+
+	bag = np.array(bag)
+	vocab_pos_freq = np.array(vocab_pos_freq)
+	vocab_neg_freq = np.array(vocab_neg_freq)
+
+	rel_scores_pos = np.multiply(bag, vocab_pos_freq)
+	rel_scores_neg = np.multiply(bag, vocab_neg_freq)
+
+	features_pos = []
+	features_neg = []
+	for i, v in enumerate(bag):
+		if v == 1:
+			features_pos.append(rel_scores_pos[i])
+			features_neg.append(rel_scores_neg[i])
+
+	final_pos = score_pos + np.sum(np.log(features_pos))
+	final_neg = score_neg + np.sum(np.log(features_neg))
+
+	print(final_pos)
+	print(final_neg)
+
+
+	#print(rel_scores_neg)
+
+	# print(score_pos)
+	# # print(np.where(rel_scores_pos != 0.0))
+	# # print(vocab[177])
+	# print(np.sum(rel_scores_pos))
+	# rel_scores_pos = np.sum(np.log(rel_scores_pos))
+	# rel_scores_neg = np.sum(np.log(rel_scores_neg))
+
+	# final_pos_score = score_pos + rel_scores_pos
+
+	# final_neg_score = score_neg + rel_scores_neg
+
+	# print(final_pos_score)
+	# print(final_neg_score)
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -6,6 +6,8 @@ TRAIN_NEG_PATH = "data/aclImdb/aclImdb/train/neg"
 
 #data_preprocessing.generate_embeddings_generic(1, 2, TRAIN_POS_PATH, TRAIN_NEG_PATH)
 
-vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq = classifiers.train_multinomial_NB(TRAIN_POS_PATH, TRAIN_NEG_PATH)
+vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq = classifiers.train_multinomial_NB(TRAIN_POS_PATH, TRAIN_NEG_PATH, False)
 
-
+doc_1 = "i really liked this movie, the performance of the actors was marvelous and enjoyed every second"
+doc = "This movie was really bad. The actors were awful and I can say that was the worst performance I have witnessed in my life"
+classifiers.apply_multinomial_NB(doc_1, vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq)
