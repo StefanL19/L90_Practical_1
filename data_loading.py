77d110ce0a119442ebdd8ae6960ec16a0719d2fc
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2019-11-04 23:45:08 +0000

    Implemented preprocessing BoW features for SVM

--- data_loading.py
+++ data_loading.py
@@ -30,10 +30,71 @@ def collect_train_data(train_data_files, stopwords):
 
 	return all_docs
 
-def load_data_kfold_10(train_pos_data_path, train_neg_data_path, stopwords, test_category):
+def load_data_kfold_10_test_val(train_pos_data_path, train_neg_data_path, stopwords, val_category, test_category):
 	"""
 		Reads the data, tokenizes it by removing the stopwords and splits it by taking 9 out of 10 folds for training and 1 fold for testing
 	"""
+
+	train_files_pos = [join(train_pos_data_path, f) for f in listdir(train_pos_data_path) if isfile(join(train_pos_data_path, f))]
+	train_files_neg = [join(train_neg_data_path, f) for f in listdir(train_neg_data_path) if isfile(join(train_neg_data_path, f))]
+
+	train_files_pos.sort(key = lambda x: x.split("_")[0].replace("cv", ""))
+	train_files_neg.sort(key = lambda x: x.split("_")[0].replace("cv", ""))
+
+	print("The count of all positive files is: ", len(train_files_pos))
+	print("The count of all negative files is: ", len(train_files_neg))
+
+	all_pos_docs = collect_train_data(train_files_pos, stopwords)
+	all_neg_docs = collect_train_data(train_files_neg, stopwords)
+
+	pos_train = []
+	neg_train = []
+	pos_val = []
+	neg_val = []
+	pos_test = []
+	neg_test = []
+	remove_pos = []
+	remove_neg = []
+
+	for idx, pos_entry in enumerate(all_pos_docs):
+		# If the index falls into the test category
+		if (idx%10) == test_category:
+			# Add the entry to the positive test set
+			pos_test.append(pos_entry)
+		elif (idx%10) == val_category:
+			# Add the entry to the positive validation set
+			pos_val.append(pos_entry)
+		else:
+			# Add the entry to the positive train set
+			pos_train.append(pos_entry)
+
+
+	# Do the same for the negative set
+	for idx, neg_entry in enumerate(all_neg_docs):
+		# If the index falls into the test category
+		if (idx%10) == test_category:
+			# Add the entry to the negative test set
+			neg_test.append(neg_entry)
+		elif (idx%10) == val_category:
+			# Add the entry to the negative validation set
+			neg_val.append(neg_entry)
+		else:
+			neg_train.append(neg_entry)
+
+	print("The size of the positive training set is: ", len(pos_train))
+	print("The size of the negative training set is: ", len(neg_train))
+	print("The size of the positive validation set is: ", len(pos_val))
+	print("The size o the negative validation set is: ", len(neg_val))
+	print("The size of the positive test set is: ", len(pos_test))
+	print("The size of the negative test set is: ", len(neg_test))
+
+	return pos_train, pos_val, pos_test, neg_train, neg_val, neg_test
+
+def load_data_kfold_10_test(train_pos_data_path, train_neg_data_path, stopwords, test_category):
+	"""
+		Reads the data, tokenizes it by removing the stopwords and splits it by taking 9 out of 10 folds for training and 1 fold for testing
+	"""
+
 	train_files_pos = [join(train_pos_data_path, f) for f in listdir(train_pos_data_path) if isfile(join(train_pos_data_path, f))]
 	train_files_neg = [join(train_neg_data_path, f) for f in listdir(train_neg_data_path) if isfile(join(train_neg_data_path, f))]
 
@@ -67,17 +128,16 @@ def load_data_kfold_10(train_pos_data_path, train_neg_data_path, stopwords, test
 	for idx, neg_entry in enumerate(all_neg_docs):
 		# If the index falls into the test category
 		if (idx%10) == test_category:
-			# Add the entry to the positive test set
+			# Add the entry to the negative test set
 			neg_test.append(neg_entry)
 		else:
 			neg_train.append(neg_entry)
-
-	for i in remove_neg:
-		del all_neg_docs[i]
 
 	print("The size of the positive training set is: ", len(pos_train))
 	print("The size of the negative training set is: ", len(neg_train))
 	print("The size of the positive test set is: ", len(pos_test))
 	print("The size of the negative test set is: ", len(neg_test))
 
-	return pos_train, pos_test, neg_train, neg_test
+	return pos_train, pos_test, neg_train,  eg_test
+
+
--- preprocess_data_svm.py
+++ preprocess_data_svm.py
@@ -49,10 +49,13 @@ def convert_embeddings_to_svm_format(embeddings, doc_class):
 train_pos_path = "data/data-tagged/POS/"
 train_neg_path = "data/data-tagged/NEG/"
 stopwords = ["\n"]
-split = 0
+
+# Define Validation and Test Categories
+VAL_CATEGORY = 1
+TEST_CATEGORY = 0
 
 # Preprocess and split the data into train and test sets
-pos_train, pos_test, neg_train, neg_test = data_loading.load_data_kfold_10(train_pos_path, train_neg_path, stopwords, split)
+pos_train, pos_val, pos_test, neg_train, neg_val, neg_test = data_loading.load_data_kfold_10_test_val(train_pos_path, train_neg_path, stopwords, VAL_CATEGORY, TEST_CATEGORY)
 
 # Files that will be used for training
 train_files = pos_train + neg_train
@@ -68,33 +71,83 @@ pos_list = m.list()
 neg_list = m.list()
 pos_list_test = m.list()
 neg_list_test = m.list()
+pos_list_validation = m.list()
+neg_list_validation = m.list()
 
 vocab_length = len(vocab)
 
-print("Started iterating positive documents")
+print("Started iterating training positive documents")
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
-   pool.map(partial(count_word_occurences, vocab_length, pos_list, vocab), pos_docs_tokenized_train)
+   pool.map(partial(count_word_occurences, vocab_length, pos_list, vocab), pos_docs_tokenized_train[:2])
 
-print("Started iterating negative documents")
+print("Started iterating training negative documents")
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
-   pool.map(partial(count_word_occurences, vocab_length, neg_list, vocab), neg_docs_tokenized_train)
+   pool.map(partial(count_word_occurences, vocab_length, neg_list, vocab), neg_docs_tokenized_train[:2])
 
 # Generate training docs SVM Embeddings and save them to a dataframe in a csv file
 pos_train_embeddings_format = convert_embeddings_to_svm_format(pos_list, 1)
-pos_train_embeddings_format.to_csv('pos_train_split_0.csv', index=False, header=False)
+pos_train_df = pd.DataFrame(pos_train_embeddings_format)
+pos_train_df.to_csv('data/pos_train_val_1_test_0.csv', index=False, header=False)
 
 neg_train_embeddings_format = convert_embeddings_to_svm_format(neg_list, 0)
-neg_train_embeddings_format.to_csv('neg_train_split_0.csv', index=False, header=False)
+neg_train_df = pd.DataFrame(neg_train_embeddings_format)
+neg_train_df.to_csv('data/neg_train_val_1_test_0.csv', index=False, header=False)
 
 
-# test_files = pos_test + neg_test
-# _, test_docs_tokenized = data_preprocessing.generate_embeddings_generic(1, 2, test_files)
 
-# pos_test_embeddings = generate_BoW_vectors(test_files[len(pos_test):])
-# pos_test_embeddings_format = convert_embeddings_to_svm_format(pos_test_embeddings, 1)
 
-# neg_test_embeddings = generate_BoW_vectors(test_files[:len(pos_test)])
-# neg_test_embeddings_format = convert_embeddings_to_svm_format(neg_test_embeddings, 0)
+# Files that will be used for validation
+val_files = pos_val + neg_val
+
+_, val_docs_tokenized = data_preprocessing.generate_embeddings_generic(1, 2, val_files)
+pos_docs_tokenized_val = val_docs_tokenized[:len(pos_val)]
+neg_docs_tokenized_val = val_docs_tokenized[len(neg_val):]
+
+print("Started iterating validation positive documents")
+with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
+   pool.map(partial(count_word_occurences, vocab_length, pos_list_validation, vocab), pos_docs_tokenized_val[:2])
+
+print("Started iterating validation negative documents")
+with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
+   pool.map(partial(count_word_occurences, vocab_length, neg_list_validation, vocab), neg_docs_tokenized_val[:2])
+
+# Generate validation docs SVM Embeddings and save them to a dataframe in a csv file
+pos_val_embeddings_format = convert_embeddings_to_svm_format(pos_list_validation, 1)
+pos_val_df = pd.DataFrame(pos_val_embeddings_format)
+pos_val_df.to_csv('data/pos_val_val_1_test_0.csv', index=False, header=False)
+
+neg_val_embeddings_format = convert_embeddings_to_svm_format(neg_list_validation, 0)
+neg_val_df = pd.DataFrame(neg_val_embeddings_format)
+neg_val_df.to_csv('data/neg_val_val_1_test_0.csv', index=False, header=False)
+
+
+
+
+# Files that will be used for testing
+test_files = pos_test + neg_test
+
+_, test_docs_tokenized = data_preprocessing.generate_embeddings_generic(1, 2, test_files)
+pos_docs_tokenized_test = test_docs_tokenized[:len(pos_test)]
+neg_docs_tokenized_test = test_docs_tokenized[len(pos_test):]
+
+print("Started iterating test positive documents")
+with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
+   pool.map(partial(count_word_occurences, vocab_length, pos_list_test, vocab), pos_docs_tokenized_test[:2])
+
+print("Started iterating test negative documents")
+with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
+   pool.map(partial(count_word_occurences, vocab_length, neg_list_test, vocab), neg_docs_tokenized_test[:2])
+
+# Generate validation docs SVM Embeddings and save them to a dataframe in a csv file
+pos_test_embeddings_format = convert_embeddings_to_svm_format(pos_list_test, 1)
+pos_test_df = pd.DataFrame(pos_test_embeddings_format)
+pos_test_df.to_csv('data/pos_test_val_1_test_0.csv', index=False, header=False)
+
+neg_test_embeddings_format = convert_embeddings_to_svm_format(neg_list_test, 0)
+neg_test_df = pd.DataFrame(neg_test_embeddings_format)
+neg_test_df.to_csv('data/neg_test_val_1_test_0.csv', index=False, header=False)
+
+
 
 
 
