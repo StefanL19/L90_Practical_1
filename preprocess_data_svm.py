2ef265bba2ac927b75167ff0f28bee84bef84c49
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2019-11-05 00:05:50 +0000

    Finished preprocessing SVM BoW data

--- preprocess_data_svm.py
+++ preprocess_data_svm.py
@@ -78,20 +78,22 @@ vocab_length = len(vocab)
 
 print("Started iterating training positive documents")
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
-   pool.map(partial(count_word_occurences, vocab_length, pos_list, vocab), pos_docs_tokenized_train[:2])
+   pool.map(partial(count_word_occurences, vocab_length, pos_list, vocab), pos_docs_tokenized_train)
 
 print("Started iterating training negative documents")
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
-   pool.map(partial(count_word_occurences, vocab_length, neg_list, vocab), neg_docs_tokenized_train[:2])
+   pool.map(partial(count_word_occurences, vocab_length, neg_list, vocab), neg_docs_tokenized_train)
 
 # Generate training docs SVM Embeddings and save them to a dataframe in a csv file
 pos_train_embeddings_format = convert_embeddings_to_svm_format(pos_list, 1)
 pos_train_df = pd.DataFrame(pos_train_embeddings_format)
-pos_train_df.to_csv('data/pos_train_val_1_test_0.csv', index=False, header=False)
+pos_train_path = 'data/pos_train_val_%d_test_%d.csv' %(VAL_CATEGORY, TEST_CATEGORY)
+pos_train_df.to_csv(pos_train_path, index=False, header=False)
 
 neg_train_embeddings_format = convert_embeddings_to_svm_format(neg_list, 0)
 neg_train_df = pd.DataFrame(neg_train_embeddings_format)
-neg_train_df.to_csv('data/neg_train_val_1_test_0.csv', index=False, header=False)
+neg_train_path = 'data/neg_train_val_%d_test_%d.csv' %(VAL_CATEGORY, TEST_CATEGORY)
+neg_train_df.to_csv(neg_train_path, index=False, header=False)
 
 
 
@@ -105,20 +107,24 @@ neg_docs_tokenized_val = val_docs_tokenized[len(neg_val):]
 
 print("Started iterating validation positive documents")
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
-   pool.map(partial(count_word_occurences, vocab_length, pos_list_validation, vocab), pos_docs_tokenized_val[:2])
+   pool.map(partial(count_word_occurences, vocab_length, pos_list_validation, vocab), pos_docs_tokenized_val)
 
 print("Started iterating validation negative documents")
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
-   pool.map(partial(count_word_occurences, vocab_length, neg_list_validation, vocab), neg_docs_tokenized_val[:2])
+   pool.map(partial(count_word_occurences, vocab_length, neg_list_validation, vocab), neg_docs_tokenized_val)
 
 # Generate validation docs SVM Embeddings and save them to a dataframe in a csv file
 pos_val_embeddings_format = convert_embeddings_to_svm_format(pos_list_validation, 1)
 pos_val_df = pd.DataFrame(pos_val_embeddings_format)
-pos_val_df.to_csv('data/pos_val_val_1_test_0.csv', index=False, header=False)
+
+pos_val_path = 'data/pos_val_val_%d_test_%d.csv' %(VAL_CATEGORY, TEST_CATEGORY)
+pos_val_df.to_csv(pos_val_path, index=False, header=False)
 
 neg_val_embeddings_format = convert_embeddings_to_svm_format(neg_list_validation, 0)
 neg_val_df = pd.DataFrame(neg_val_embeddings_format)
-neg_val_df.to_csv('data/neg_val_val_1_test_0.csv', index=False, header=False)
+
+neg_val_path = 'data/neg_val_val_%d_test_%d.csv' %(VAL_CATEGORY, TEST_CATEGORY)
+neg_val_df.to_csv(neg_val_path, index=False, header=False)
 
 
 
@@ -132,22 +138,24 @@ neg_docs_tokenized_test = test_docs_tokenized[len(pos_test):]
 
 print("Started iterating test positive documents")
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
-   pool.map(partial(count_word_occurences, vocab_length, pos_list_test, vocab), pos_docs_tokenized_test[:2])
+   pool.map(partial(count_word_occurences, vocab_length, pos_list_test, vocab), pos_docs_tokenized_test)
 
 print("Started iterating test negative documents")
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()-1) as pool:
-   pool.map(partial(count_word_occurences, vocab_length, neg_list_test, vocab), neg_docs_tokenized_test[:2])
+   pool.map(partial(count_word_occurences, vocab_length, neg_list_test, vocab), neg_docs_tokenized_test)
 
 # Generate validation docs SVM Embeddings and save them to a dataframe in a csv file
 pos_test_embeddings_format = convert_embeddings_to_svm_format(pos_list_test, 1)
 pos_test_df = pd.DataFrame(pos_test_embeddings_format)
-pos_test_df.to_csv('data/pos_test_val_1_test_0.csv', index=False, header=False)
+pos_test_path = 'data/pos_test_val_%d_test_%d.csv' %(VAL_CATEGORY, TEST_CATEGORY)
+pos_test_df.to_csv(pos_test_path, index=False, header=False)
 
 neg_test_embeddings_format = convert_embeddings_to_svm_format(neg_list_test, 0)
 neg_test_df = pd.DataFrame(neg_test_embeddings_format)
-neg_test_df.to_csv('data/neg_test_val_1_test_0.csv', index=False, header=False)
+neg_test_path = 'data/neg_test_val_%d_test_%d.csv' %(VAL_CATEGORY, TEST_CATEGORY)
+neg_test_df.to_csv(neg_test_path, index=False, header=False)
 
 
-
-
+# pos_tr = pd.read_csv("data/pos_train_val_1_test_0.csv", header=None)
+# print(pos_tr.values[0][0])
 
