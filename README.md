36bc30ff741aec6ae1d6fd593716105ed7236422
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2019-11-13 09:24:28 +0000

    Training Laplace Smoothing  No unigrams Use Bigrams

--- README.md
+++ README.md
@@ -3,9 +3,11 @@ The code for the practical for the L90 course
 
 # Files and organization
 ### Train  a Naive Bayes classifier on cv000-cv899 and test on cv900-cv999
-train_nb_no_fold.py
+Use the file train_nb_no_fold.py and set the Laplace parameter to False 
 
 ### Train a Naive Bayes classifier + Laplace Smoothing on cv000-cv899 and test on cv900-cv999 
+Use the file train_nb_no_fold.py and set the Laplace parameter to True
+
 ### Sign Test No smoothing vs Smoothing
 ### 10-fold Round Robin Cross Validation + Laplace Smoothing
 ### 10-fold Round Robin Cross Validation No Smoothing 
--- train_nb_no_kfold.py
+++ train_nb_no_kfold.py
@@ -11,17 +11,16 @@ LAPLACE_SMOOTHING=True
 STOPWORDS = ["\n"]
 TRAIN_POS_PATH = "data/data-tagged/POS/"
 TRAIN_NEG_PATH = "data/data-tagged/NEG/"
-USE_UNIGRAMS = True
+USE_UNIGRAMS = False
 USE_BIGRAMS = True
 
 TRAIN_NEW = True
-OUT_PATH = "data/trained_models/no_fold_unigram_true_bigram_true_laplace_true/"
-
+OUT_PATH = "data/trained_models/no_fold_unigram_false_bigram_true_laplace_true/"
 
 import os
 if not os.path.exists(OUT_PATH):
     os.makedirs(OUT_PATH)
-  
+
 # #Parameter that will determine which files are we going to use for testing
 # TEST_CATEGORY  = 9
 
@@ -35,14 +34,25 @@ if TRAIN_NEW:
 else:
 	vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq = classifiers.load_nb_model(OUT_PATH)
 
+
+top_10_idx = np.argsort(vocab_pos_freq)[-100:]
+top_10_values = [vocabulary[i] for i in top_10_idx]
+print("Top positive words: ", top_10_values)
+print("-------------------------------------------")
+
+top_10_idx = np.argsort(vocab_neg_freq)[-100:]
+top_10_values = [vocabulary[i] for i in top_10_idx]
+print("Top negative words: ", top_10_values)
+print("-------------------------------------------")
+
 # Generate the predictions by using a saved model
 m = multiprocessing.Manager()
 preds = m.list()
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()- 1) as pool:
-    pool.map(partial(classifiers.apply_multinomial_NB, vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq, 1, preds, 1, 1), pos_test)
+    pool.map(partial(classifiers.apply_multinomial_NB, vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq, 1, preds, 2, 2), pos_test)
 
 with multiprocessing.Pool(processes=multiprocessing.cpu_count()- 1) as pool:
-    pool.map(partial(classifiers.apply_multinomial_NB, vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq, 0, preds, 1, 1), neg_test)
+    pool.map(partial(classifiers.apply_multinomial_NB, vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq, 0, preds, 2, 2), neg_test)
 
 all_gt = np.array(preds)[:, 0]
 all_preds = np.array(preds)[:, 1]
@@ -50,4 +60,7 @@ all_preds = np.array(preds)[:, 1]
 overall_accuracy = metrics.acc(all_preds, all_gt)
 print("The overall accuracy of the model is: ", overall_accuracy)
 
-classifiers.save_nb_classifier(vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq, OUT_PATH)
+if TRAIN_NEW:
+	classifiers.save_nb_classifier(vocabulary, prior_pos, prior_neg, vocab_pos_freq, vocab_neg_freq, OUT_PATH)
+
+
