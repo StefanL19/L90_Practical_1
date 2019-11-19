18a0a07c1e3a5e8d30ee49e7713fefc40c5fefe4
Stefan Lazov <stefanlazov@Stefans-MacBook-Pro.local>
2019-11-19 22:24:59 +0000

    no changes

--- calculate_metrics.py
+++ calculate_metrics.py
@@ -28,8 +28,8 @@ def significance_test_nfold():
 	gt_total = []
 	for i in range(0,10):
 		gt = [1]*100 + [0]*100
-		system_a_pred_fold = np.loadtxt("data/trained_models/predictions/cross_fold/"+str(i)+"/unigram_true_bigram_true_laplace_false_stopwords_false.txt")
-		system_b_pred_fold = np.loadtxt("data/trained_models/predictions/cross_fold/"+str(i)+"/unigram_true_bigram_true_laplace_true_stopwords_false.txt")
+		system_a_pred_fold = np.loadtxt("data/trained_models/predictions/cross_fold/"+str(i)+"/unigram_false_bigram_true_laplace_false_stopwords_false.txt")
+		system_b_pred_fold = np.loadtxt("data/trained_models/predictions/cross_fold/"+str(i)+"/unigram_true_bigram_false_laplace_false_stopwords_false.txt")
 		#print("Significance test result: ", metrics.sign_test_precision(system_a_pred_fold, system_b_pred_fold, gt))
 
 		system_a_pred_total.extend(system_a_pred_fold)
@@ -39,16 +39,35 @@ def significance_test_nfold():
 
 	print("Significance test result: ", metrics.sign_test_precision(system_a_pred_total, system_b_pred_total, gt_total))
 
-#significance_test_nfold()
-all_unigrams = []
-all_bigrams = []
-for i in range(0, 10):
-	c_unigrams, c_bigrams = metrics.vocabulary_stats("data/trained_models_new/10_fold_no_test/unigram_true_bigram_true_laplace_true_stopwords_false/val_fold_"+str(i)+"/vocab.txt")
-	all_unigrams.append(c_unigrams)
-	all_bigrams.append(c_bigrams)
+def significance_test_holdout():
+	system_a_pred = np.loadtxt("data/trained_models/predictions_old/no_fold/unigram_false_bigram_true_laplace_true.txt")
+	system_b_pred = np.loadtxt("data/trained_models/predictions_old/no_fold/unigram_false_bigram_true_laplace_true.txt")
 
-print("The average count of the unigrams is: ", np.mean(all_unigrams))
-print("The average count of the bigrams is: ", np.mean(all_bigrams))
+	gt = [1]*100 + [0]*100
+
+	sign_test_result = metrics.sign_test_precision(system_a_pred, system_b_pred, gt)
+	system_a_acc = metrics.acc(system_a_pred, gt)
+	system_b_acc = metrics.acc(system_b_pred, gt)
+
+	print("The accuracy of system A is: ", system_a_acc)
+	print("The accuracy of system B is: ", system_b_acc)
+	print("The result of the sign test is: ", sign_test_result)
+
+def dictionary_stats():
+	all_unigrams = []
+	all_bigrams = []
+	for i in range(0, 10):
+		c_unigrams, c_bigrams = metrics.vocabulary_stats("data/trained_models_new/10_fold_no_test/unigram_true_bigram_true_laplace_true_stopwords_false/val_fold_"+str(i)+"/vocab.txt")
+		all_unigrams.append(c_unigrams)
+		all_bigrams.append(c_bigrams)
+
+	print("The average count of the unigrams is: ", np.mean(all_unigrams))
+	print("The average count of the bigrams is: ", np.mean(all_bigrams))
+
+significance_test_nfold()
+
+#significance_test_nfold()
+
 
 
 
